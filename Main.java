a431af59ca5409bdb8877e1d4769802dbef1cadc
Jami735 <jrojasalmeda100@student.hbuhsd.edu>
2025-10-23 18:19:47 +0000

    findMax

--- Main.java
+++ Main.java
@@ -5,5 +5,30 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+   
+    String word = Mississippi;
+    int maxCount = 0;
+    String maxLetter = "";
+
+    for (int count = 0; count < word.length; count++)
+    {
+      String target = word.substring(i, i+1);
+      int count = 0;
+      for (int num = 0; num < word.length(); num++)
+      {
+        String currentLetter = word.substring(num, num+1);
+        if (currentLetter.equals(target))
+        {
+          count++;
+        }
+        
+      }
+      System.out.println("The letter " + target + "appears " + count "time");
+    }
+
+    if (maxCount < maxLetter)
+    {
+      
+    }
   }
 }
